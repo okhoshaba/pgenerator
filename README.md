675027931efc1328c58ec43e20defdb2f4de50ef
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2022-06-19 19:21:22 +0300

    change some files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # python generator for services
 
+
+keywords:
+python fourier transform tutorial
+
+keywords:
+python concurrent execution
+https://www.tutorialspoint.com/concurrency_in_python/concurrency_in_python_quick_guide.htm
+
+keywords:
+python command execution
+
---
+++ gen01.py
@@ -0,0 +1,34 @@
+# Examples 01
+# From https://www.tutorialspoint.com/concurrency_in_python/concurrency_in_python_quick_guide.htm
+
+import _thread
+import time
+
+
+def run_amplitude( threadName, delay, amplitude):
+   count = 0
+   while count < amplitude:
+#   while count < 5:
+      time.sleep(delay)
+      count += 1
+      print ("%s, %s: %s" % ( threadName, count, time.ctime(time.time()) ))
+
+try:
+   _thread.start_new_thread(run_amplitude, ("Amplitude = 1", 0.2, 10, ) )
+#   print("End generator 1")
+   time.sleep(0.2)
+   _thread.start_new_thread(run_amplitude, ("Amplitude == 2", 0.4, 5, ) )
+
+#   print("End generator 2")
+except:
+   print ("Error run generator")
+
+# print("End generator")
+
+
+print("End generatorAll")
+
+# Wait for End process
+time.sleep(3)
+while 0:
+   pass
