899c00c753440fa333617ba8de414b442b6e9f50
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2022-06-22 16:26:39 +0300

    add loadimpact

--- gen04.py
+++ gen04.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as loadTrajectory
 import scipy.fftpack
+import math
 
 basis = 20
 amplitudes = 10
@@ -19,7 +20,9 @@ dt = 1/Fs   # Период времени
 Fc = 5
 sample = 1000
 t = np.arange(sample)
+loadImpact = np.arange(sample)
 responseTime = np.arange(sample)
+#procTime = np.arange(sample)
 
 quer = basis + np.sin(2 * np.pi * Fc * t / Fs) * amplitudes
 processingTime = 0.1 / quer
@@ -28,6 +31,7 @@ processingTime = 0.1 / quer
 def runAmplitude(threadName, amplitude):
    intCount = 0
    while intCount < amplitude:
+      os.system("curl 192.168.1.108:9000 > /dev/null 2>&1")
       intCount += 1
 #     For diagnose only
 #      print("amplitude = %s" % (intCount))
@@ -39,11 +43,15 @@ try:
       startTime = time.time_ns()
       _thread.start_new_thread(runAmplitude, (extCount, int(quer[extCount]), ) )
       endTime = time.time_ns()
-      os.system("curl 192.168.1.108:9000 > /dev/null 2>&1")
+      responseTime[extCount] = endTime - startTime
+      rp = responseTime[extCount]/1000000
+      loadImpact[extCount] = math.log10(0.1/rp) * 1000
+#      procTime[extCount] = 0.1 / quer[extCount]
+#      print("%s, %s, %s, %s " % (extCount, int(quer[extCount]), procTime[extCount], responseTime[extCount]))
+      print("%s, %s, %s, %s, %s, %s, %s" % (extCount, int(quer[extCount]), 0.1, responseTime[extCount]/1000000, 0.1/rp, math.log10(0.1/rp)*10, loadImpact[extCount]))
+      time.sleep(0.1)
 #      os.system("curl 192.168.222.19:9000 > /dev/null 2>&1")
       #os.system("echo curl")
-      responseTime[extCount] = endTime - startTime
-      time.sleep(0.1)
 #     For diagnose only
 #      os.system("echo curl")
 
@@ -53,7 +61,7 @@ except:
 # Build a graph
 loadTrajectory.plot(quer)
 loadTrajectory.title('Load Trajectory')
-loadTrajectory.xlabel('Time (in sec)')
+loadTrajectory.xlabel('Time (in msec)')
 loadTrajectory.ylabel('Amplitude (in queries)')
 loadTrajectory.show()
 
@@ -62,13 +70,15 @@ loadTrajectory.show()
 x2 = np.linspace(0.0, 1, Fs)
 #y2 = amplitudes * np.sin(2 * np.pi * Fc * x2)
 #yf2 = scipy.fftpack.fft(y2)
-yf2 = scipy.fftpack.fft(responseTime)
+#yf2 = scipy.fftpack.fft(math.log10(responseTime/processingTime))
+# !! yf2 = scipy.fftpack.fft(processingTime)
 xf2 = np.linspace(0.0, 1.0/(2.0*dt), Fs//2)
+yf2 = scipy.fftpack.fft(loadImpact)
 ymax = max(2.0/Fs * np.abs(yf2[:Fs//2]))
 print(ymax)
-#print(responseTime)
 
 fig, ax = plt.subplots()
+
 # Plotting only the left part of the spectrum to not show aliasing
 #ax.plot(xf1, 2.0/N * np.abs(yf1[:N//2]), label='fftpack tutorial')
 ax.plot(xf2, 2.0/Fs * np.abs(yf2[:Fs//2]), label='Integer number of periods')
