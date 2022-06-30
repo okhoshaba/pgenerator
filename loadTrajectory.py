eca1645d40e0ec81ba5df23e36a5fa411d0a0032
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2022-06-30 22:14:12 +0300

    change processing time

--- loadTrajectory.py
+++ loadTrajectory.py
@@ -14,15 +14,17 @@ import numpy as np
 import matplotlib.pyplot as loadTrajectory
 import math
 
-basis = 20
-amplitudes = 9
+basis = 2000
+amplitudes = 500
 Fs = 100
-dt = 1/Fs   # Период времени
+#dt = 1/Fs   # Период времени
 Fc = 20
 sample = 100
 t = np.arange(sample)
-#loadImpact = np.arange(sample)
 loadImpact = np.arange(0.0,100.0,1.0)
+RT = np.arange(0.0,100.0,1.0)
+bnx = (1,50,100)
+bny = (0.1,0.1,0.1)
 
 quer = basis + np.sin(2 * np.pi * Fc * t / Fs) * amplitudes
 
@@ -30,10 +32,11 @@ quer = basis + np.sin(2 * np.pi * Fc * t / Fs) * amplitudes
 def runAmplitude(threadName, amplitude):
    intCount = 0
    while intCount < amplitude:
+#      os.system("curl 192.168.1.108:9000 > /dev/null 2>&1")
       os.system("ping -c1 192.168.1.1 > /dev/null 2>&1")
 #      os.system("ping -c1 192.168.222.19 > /dev/null 2>&1")
 #     For web server
-#      os.system("curl curl 192.168.222.19:9000 > /dev/null 2>&1")
+#      os.system("curl 192.168.222.19:9000 > /dev/null 2>&1")
       intCount += 1
 #     For diagnose only
 #      print("amplitude = %s" % (intCount))
@@ -47,19 +50,21 @@ try:
       startTime = time.time_ns()
       _thread.start_new_thread(runAmplitude, (extCount, int(quer[extCount]), ) )
       endTime = time.time_ns()
-      newRT = (endTime - startTime)/1000000.0
+# in ms      
+      RT[extCount] = (endTime - startTime)/1000000
+#      RT = (endTime - startTime)/1000000
 #      newLI = math.log10(newRT/newPT)*10.0
 #!!!  If PT = 0.1 and coef = 10, we use:
 #      newLI = math.log10(newRT)
 #      loadImpact[extCount] = math.log10(newRT) * 1.0
-      loadImpact[extCount] = newRT
+      loadImpact[extCount] = math.log10(RT[extCount])
 #      newLI = math.log10(newRT/0.1)*10.0
 #      loadImpact[extCount] = newLI
 #     For diagnose only
 #      print("%s, %s, %s" % (newRT, newPT, newLI))
-      print("%s, %s, %s" % (newRT, 0.1, loadImpact[extCount]))
+      print("%s, %s, %s" % (RT[extCount], 0.01, loadImpact[extCount]))
 
-      time.sleep(0.1)
+      time.sleep(0.01)
 #     For diagnose only
 #      os.system("curl 192.168.222.19:9000 > /dev/null 2>&1")
       #os.system("echo curl")
@@ -78,9 +83,12 @@ loadTrajectory.ylabel('Amplitude (in queries)')
 loadTrajectory.legend(loc='best')
 
 loadTrajectory.subplot(2,1,2)
-loadTrajectory.plot(t,loadImpact,'b-',label='RT')
+loadTrajectory.plot(t,RT,'b-',label='RT')
+#loadTrajectory.plot(t,loadImpact,'b-',label='loadImpact')
+#loadTrajectory.plot(bnx, bny, 'r-', label='Bottleneck')
 #loadTrajectory.plot(0.1,'r-',label='RT')
 loadTrajectory.legend(loc='best')
+
 loadTrajectory.show()
 
 # Wait for End process
