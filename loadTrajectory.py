a8ed9451e4bc9e471dbca2d816508ef56e79f90e
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2022-06-30 17:17:52 +0300

    add graphic

--- loadTrajectory.py
+++ loadTrajectory.py
@@ -7,12 +7,11 @@ from cProfile import label
 from concurrent.futures import process
 import time
 import os
+from unicodedata import decimal
 from urllib import response
 from xml.etree.ElementTree import ProcessingInstruction
 import numpy as np
-#import matplotlib.pyplot as plt
 import matplotlib.pyplot as loadTrajectory
-import scipy.fftpack
 import math
 
 basis = 20
@@ -22,7 +21,8 @@ dt = 1/Fs   # Период времени
 Fc = 20
 sample = 100
 t = np.arange(sample)
-loadImpact = np.arange(sample)
+#loadImpact = np.arange(sample)
+loadImpact = np.arange(0.0,100.0,1.0)
 
 quer = basis + np.sin(2 * np.pi * Fc * t / Fs) * amplitudes
 
@@ -43,19 +43,21 @@ try:
     # External cycles = extCount
 # !!   print ("begin: -- %s --" % (time.ctime(time.time()) ))
    for extCount in range(sample):
+      loadImpact[extCount] = 0.0
       startTime = time.time_ns()
       _thread.start_new_thread(runAmplitude, (extCount, int(quer[extCount]), ) )
       endTime = time.time_ns()
-      newRT = (endTime - startTime)/1000000
-#      newPT = 0.1/quer[extCount]
+      newRT = (endTime - startTime)/1000000.0
 #      newLI = math.log10(newRT/newPT)*10.0
 #!!!  If PT = 0.1 and coef = 10, we use:
-      newLI = math.log10(newRT)
+#      newLI = math.log10(newRT)
+#      loadImpact[extCount] = math.log10(newRT) * 1.0
+      loadImpact[extCount] = newRT
 #      newLI = math.log10(newRT/0.1)*10.0
 #      loadImpact[extCount] = newLI
 #     For diagnose only
 #      print("%s, %s, %s" % (newRT, newPT, newLI))
-# !!!      print("%s, %s, %s" % (newRT, 0.1, newLI))
+      print("%s, %s, %s" % (newRT, 0.1, loadImpact[extCount]))
 
       time.sleep(0.1)
 #     For diagnose only
@@ -76,7 +78,9 @@ loadTrajectory.ylabel('Amplitude (in queries)')
 loadTrajectory.legend(loc='best')
 
 loadTrajectory.subplot(2,1,2)
-loadTrajectory.plot(t,newRT,'b-',label='RT')
+loadTrajectory.plot(t,loadImpact,'b-',label='RT')
+#loadTrajectory.plot(0.1,'r-',label='RT')
+loadTrajectory.legend(loc='best')
 loadTrajectory.show()
 
 # Wait for End process
